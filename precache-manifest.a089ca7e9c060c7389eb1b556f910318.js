d3e02cabf44f8899a87eba5fe9bdb9de5322cd2a
shyamrendra <shyamrendra@gmail.com>
2019-12-28 17:18:46 +0530

    Updates

---
+++ precache-manifest.a089ca7e9c060c7389eb1b556f910318.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889f9743988eed32671309e5a5134726",
+    "url": "/apps1/index.html"
+  },
+  {
+    "revision": "2e7cca84d2823a8050e1",
+    "url": "/apps1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7a2eafe8506095aed546",
+    "url": "/apps1/static/js/2.0e8005eb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/apps1/static/js/2.0e8005eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e7cca84d2823a8050e1",
+    "url": "/apps1/static/js/main.a8dba866.chunk.js"
+  },
+  {
+    "revision": "885950a4f144bae356c9",
+    "url": "/apps1/static/js/runtime-main.3a147234.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/apps1/static/media/logo.5d5d9eef.svg"
+  }
+]);
